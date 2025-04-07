94d19da43d8c3b3dc9be8dc76617227a416c0e85
Shubhojit-Mitra <shubhojit.120225@stu.upes.ac.in>
2025-04-08 04:00:27 +0530

    create typewritter effect

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,40 @@
+"use client";
+
 import Image from "next/image";
+import { useEffect, useRef } from "react";
+import Typed from "typed.js";
 
 export default function Home() {
+
+  const el = useRef(null);
+
+    useEffect(() => {
+      const typed = new Typed(el.current, {
+        strings: ['Coding', 'Web Development', 'Full Stack', 'UI/UX Design', 'Next.js'],
+        typeSpeed: 90,
+        backSpeed: 50,
+        backDelay: 3000,
+        loop: true,
+        loopCount: Infinity,
+      });
+  
+      return () => {
+        // Destroy Typed instance during cleanup to stop animation
+        typed.destroy();
+      };
+    }, []); 
+
   return (
     <main>
       <section className="container px-4 py-10 mx-auto lg:h-128 lg:space-x-8 lg:flex lg:items-center">
         <div className="w-full text-center lg:text-left lg:w-1/2 lg:-mt-8 border-b-2 pb-10">
           <h1 className="text-3xl leading-snug text-gray-800 dark:text-gray-200 md:text-4xl">
-            A <span className="font-semibold">free repository</span> for community <br className="hidden lg:block" /> components using <span className="font-semibold underline decoration-primary">
-              {/* <span ref={el} /> */}
+            A <span className="font-semibold">free repository</span> for community <br className="hidden lg:block" /> posts about <br /> <span className="font-semibold underline decoration-primary">
+              <span ref={el} />
               </span>
           </h1>
           <p className="mt-4 text-lg text-gray-500 dark:text-gray-300">
-            Open source Tailwind UI components and templates to <br className="hidden lg:block" /> bootstrap your new apps, projects or landing sites!
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa, placeat. <br className="hidden lg:block" /> Lorem ipsum dolor, sit amet consectetur adipisicing elit.
           </p>
         </div>
         <div className="w-full mt-4 lg:mt-0 lg:w-1/2">

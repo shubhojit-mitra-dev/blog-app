b39f809268dc5a76ee9d9b30968ed01fccb0b995
Shubhojit-Mitra <shubhojit.120225@stu.upes.ac.in>
2025-04-16 04:11:45 +0530

    fix build errors

--- src/components/home/BlogCard.tsx
+++ src/components/home/BlogCard.tsx
@@ -1,5 +1,5 @@
 import Image from "next/image";
-import { Button, buttonVariants } from "@/components/ui/button";
+import { buttonVariants } from "@/components/ui/button";
 import { StaticImageData } from "next/image";
 import Link from "next/link";
 

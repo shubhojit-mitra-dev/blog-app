4c8dd1b1233a8d6fd82ead150ee14f4ac134da7b
Shubhojit-Mitra <shubhojit.120225@stu.upes.ac.in>
2025-05-06 13:02:20 +0530

    fix: add line clamping to blog description for improved text overflow handling

--- src/components/home/BlogCard.tsx
+++ src/components/home/BlogCard.tsx
@@ -22,7 +22,7 @@ export default function BlogCard({ image, title, description }: BlogCardProps) {
         />
         <div className="mt-4">
           <h3 className="text-xl font-semibold text-gray-800 dark:text-gray-200 line-clamp-1 overflow-hidden text-ellipsis">{title}</h3>
-          <p className="mt-2 mb-8 text-gray-600 dark:text-gray-400">{description}</p>
+          <p className="mt-2 mb-8 text-gray-600 dark:text-gray-400 line-clamp-2 overflow-hidden text-ellipsis">{description}</p>
           <Link href={`/blogs/${title.replace(/\s+/g, '-').toLowerCase()}`} className={buttonVariants({ variant: "outline"})}>Read More
           </Link>
         </div>
